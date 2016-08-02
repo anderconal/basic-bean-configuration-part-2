4febb3ec1bdf0f095ae4d135906f70e862fbd70c
Ander Conal <ander.conal@gmail.com>
2016-08-02 18:58:50 +0200

    Using arbitrary maps as bean properties

--- src/main/java/es/anderconal/modelo/Pokedex.java
+++ src/main/java/es/anderconal/modelo/Pokedex.java
@@ -5,11 +5,15 @@ import java.util.Map;
 
 public class Pokedex {
 	private Map<String, String> tipos = new HashMap<String, String>();
+	private Map<String, Pokemon> pokemon = new HashMap<String, Pokemon>();
 
 	public void setTipos(Map<String, String> tipos) {
 		this.tipos = tipos;
 	}
-
+	
+	public void setPokemon(Map<String, Pokemon> pokemon) {
+		this.pokemon = pokemon;
+	}
 
 	@Override
 	public String toString() {
@@ -18,6 +22,13 @@ public class Pokedex {
 		for (Map.Entry<String, String> entrada : tipos.entrySet()) {
 			sb.append(entrada.getKey() + ": " + entrada.getValue() + "\n");
 		}
+		
+		sb.append("\n");
+		
+		for (Map.Entry<String, Pokemon> entrada : pokemon.entrySet()) {
+			sb.append(entrada.getKey() + ": " + entrada.getValue());
+		}
+		
 		return sb.toString();
 	}
 }
